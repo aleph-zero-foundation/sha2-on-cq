df5f287fbba691b2ec6a7c29ecb27b82e344efcc
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-05-20 16:19:27 +0200

    Indexing trace

--- src/trace.rs
+++ src/trace.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use crate::{
     constants::{INITIAL_HASH_WORDS, ROUND_CONSTANTS},
     types::{right_rotation, Word, Worimb},
@@ -10,10 +12,10 @@ pub struct Trace {
 
     majority: [Worimb; ROUNDS],
     choose: [Worimb; ROUNDS],
-    rotations: [[Word; ROUNDS]; 2],
+    rotations: [[Worimb; ROUNDS]; 2],
 
-    k: [Word; ROUNDS],
-    w: [Word; ROUNDS],
+    k: [Worimb; ROUNDS],
+    w: [Worimb; ROUNDS],
 }
 
 impl Trace {
@@ -26,8 +28,8 @@ impl Trace {
             rotations,
             majority,
             choose,
-            k: ROUND_CONSTANTS,
-            w,
+            k: ROUND_CONSTANTS.map(Worimb::from),
+            w: w.map(Worimb::from),
         }
     }
 
@@ -58,12 +60,12 @@ impl Trace {
     ) -> (
         [[Worimb; ROUNDS]; 8],
         [[Worimb; ROUNDS]; 2],
-        [[Word; ROUNDS]; 2],
+        [[Worimb; ROUNDS]; 2],
     ) {
         let mut words = [[Worimb::default(); ROUNDS]; 8];
 
         let [mut maj, mut ch] = [[Worimb::default(); ROUNDS]; 2];
-        let mut rotations = [[Word::default(); ROUNDS]; 2];
+        let mut rotations = [[Worimb::default(); ROUNDS]; 2];
 
         let mut round_input = INITIAL_HASH_WORDS.map(Worimb::from);
         for round in 0..ROUNDS {
@@ -82,7 +84,7 @@ impl Trace {
         (words, [maj, ch], rotations)
     }
 
-    fn do_round(w: Word, k: Word, words: [Worimb; 8]) -> ([Worimb; 8], [Worimb; 2], [Word; 2]) {
+    fn do_round(w: Word, k: Word, words: [Worimb; 8]) -> ([Worimb; 8], [Worimb; 2], [Worimb; 2]) {
         let [a, b, c, d, e, f, g, h] = words;
 
         let maj = (a.word & b.word) ^ (a.word & c.word) ^ (b.word & c.word);
@@ -103,17 +105,58 @@ impl Trace {
 
         (
             [
-                Worimb::from(temp1.wrapping_add(temp2)),
+                temp1.wrapping_add(temp2).into(),
                 a,
                 b,
                 c,
-                Worimb::from(d.word.wrapping_add(temp1)),
+                d.word.wrapping_add(temp1).into(),
                 e,
                 f,
                 g,
             ],
-            [Worimb::from(maj), Worimb::from(ch)],
-            [rot0, rot1],
+            [maj.into(), ch.into()],
+            [rot0.into(), rot1.into()],
         )
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub enum TraceItem {
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    maj,
+    ch,
+    rot0,
+    rot1,
+    k,
+    w,
+}
+
+impl Index<TraceItem> for Trace {
+    type Output = [Worimb; ROUNDS];
+
+    fn index(&self, item: TraceItem) -> &Self::Output {
+        match item {
+            TraceItem::a => &self.round_outputs[0],
+            TraceItem::b => &self.round_outputs[1],
+            TraceItem::c => &self.round_outputs[2],
+            TraceItem::d => &self.round_outputs[3],
+            TraceItem::e => &self.round_outputs[4],
+            TraceItem::f => &self.round_outputs[5],
+            TraceItem::g => &self.round_outputs[6],
+            TraceItem::h => &self.round_outputs[7],
+            TraceItem::maj => &self.majority,
+            TraceItem::ch => &self.choose,
+            TraceItem::rot0 => &self.rotations[0],
+            TraceItem::rot1 => &self.rotations[1],
+            TraceItem::k => &self.k,
+            TraceItem::w => &self.w,
+        }
     }
 }
